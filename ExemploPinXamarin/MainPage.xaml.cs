820f8124630f298dca5bb806b4afc015fc156ca9
Thiago Bertuzzi <thiago.bertuzzi@gmail.com>
2020-05-28 12:08:16 -0300

    :tada: Subindo exemplo pro github

---
+++ ExemploPinXamarin/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ExemploPinXamarin
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void PINView_PINEntryCompleted(System.Object sender, XFPINView.Helpers.PINCompletedEventArgs e)
+        {
+            //Este Evento determina quando o Pin for todo preenchido
+        }
+    }
+}
---
+++ ExemploPinXamarin/ViewModels/MainViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ExemploPinXamarin.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        protected bool SetProperty<T>(ref T backingStore, T value,
+             [CallerMemberName] string propertyName = "",
+             Action onChanged = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(backingStore, value))
+                return false;
+
+            backingStore = value;
+            onChanged?.Invoke();
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            var changed = PropertyChanged;
+            if (changed == null)
+                return;
+
+            changed.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+
+        string pin;
+        public string PIN
+        {
+            get { return pin; }
+            set { SetProperty(ref pin, value); }
+        }
+
+        public MainViewModel()
+        {
+        }
+    }
+}
